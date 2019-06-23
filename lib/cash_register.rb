5ee9aad95f7a2f21aab4e2b432f7310405c8ee23
Taylor Doyle <tcd56@cornell.edu>
2019-06-23 21:34:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+class CashReg 
+  attr_accessor :discount, :total, :items, :last_item
 
+  def initialize(discount = 0)
+    @total = 0
+    @items = []
+    @discount = discount
+  end
+
+  def apply_discount
+    if @discount == 0 
+      "There is no discount to apply."
+    else
+      num = self.discount.to_f / 100
+      discount_rate = self.total * num
+      self.total -= discount_rate
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+
+
+  def add_item(item, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times do
+      self.items << item
+    end
+    self.last_item = price * quantity
+  end
+
+  def void_last_transaction
+    self.total -= self.last_item
+  end
+
+end
+
+    
+    
